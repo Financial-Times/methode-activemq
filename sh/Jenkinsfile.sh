b0fe3297e1240f6a8595aba8b02ca92096d04d88
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-07-04 15:28:32 +0100

    Add environment handling

--- sh/Jenkinsfile.sh
+++ sh/Jenkinsfile.sh
@@ -4,13 +4,24 @@
 #
 # © jussi.heinonen@ft.com - 4.7.2017
 
-ENV_PROD="methode-activemq.prod.cms.in.ft.com"
-ENV_TEST="methode-activemq.test.cms.in.ft.com"
+DNS_TLD="cms.in.ft.com"
+DNS_PROD="methode-activemq.prod.${DNS_TLD}"
+DNS_TEST="methode-activemq.test.${DNS_TLD}"
+
 
 echo "Project ${GIT_URL}, branch ${GIT_BRANCH} triggered a build"
 REPO_LIST=( $(echo ${GIT_URL} | tr '/' ' ') )
 REPO=$(echo ${REPO_LIST[-1]} | cut -d '.' -f 1)
 BRANCHNAME=$(echo ${GIT_BRANCH} | cut -d '/' -f 2)
-echo "Creating stack ${REPO}-${BRANCHNAME}"
+
+if [[ "${BRANCHNAME}" == "master" ]]; then
+  echo "Deploying to ${DNS_PROD}"
+elif [[ "${BRANCHNAME}" =~ "test_" ]]; then
+  echo "Deploying to ${DNS_TEST}"
+else
+  echo "Deploying to ${REPO}.${BRANCHNAME}.${DNS_TLD}"
+fi
+
+
 
 #ssh -o StrictHostKeyChecking=no -i ~/.ssh/jenkins_id_rsa 10.170.37.239 "cd /opt/methode/methode-activemq && sudo git pull && sudo ./pipeline.sh"
