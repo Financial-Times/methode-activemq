d36abe39604e9f16ec5172323b51cf8ac5756763
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-08-01 16:02:52 +0100

    Modify function to write into Application log file in addition to stdout

--- ps/functions.ps1
+++ ps/functions.ps1
@@ -1,12 +1,15 @@
 function error([string]$message) {
   Write-Host "$message" -foreground "Red"
+  EventCreate /t Error /id 69 /l Application /so start-prime /d "$message"
 }
 
 function errorAndExit([string]$message, [int]$code) {
   Write-Host "$message. Exit $code" -foreground "Red"
+  EventCreate /t Error /id 69 /l Application /so start-prime /d "$message"
   exit $code
 }
 
 function info([string]$message) {
   Write-Host "$message" -foreground "Blue"
+  EventCreate /t Information /id 69 /l Application /so start-prime /d "$message"
 }
