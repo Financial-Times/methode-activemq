ca5a14e8427f871f5c2dc042c7fdb1b5d49279eb
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-09-20 11:17:30 +0100

    Link PROD instance with Methode PROD ActiveMQ

--- ps/start-prime.ps1
+++ ps/start-prime.ps1
@@ -26,9 +26,9 @@ else {
   # Set environment for AMQ endpoint based on 2 last characters of hostname
   $last_two = $hostname.Substring($hostname.Length - 2)
   switch ($last_two) {
-    '-p' { $amq_env = "test" }
+    '-p' { $amq_env = "prod" }
     '-t' { $amq_env = "test" }
-    'nt' { $amq_env = "test" }
+    'nt' { $amq_env = "int" }
   }
 }
 
