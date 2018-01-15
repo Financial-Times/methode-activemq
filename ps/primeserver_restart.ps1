3b4171054e27a445129b1d7924930b4aa9bec4cd
Jussi.Heinonen <jussi.heinonen@ft.com>
2018-01-15 14:50:53 +0000

    Add CloudWatch monitoring for restart

--- ps/primeserver_restart.ps1
+++ ps/primeserver_restart.ps1
@@ -6,6 +6,13 @@
 #
 #  Script requires  _AWS Tools for Powershell_, see https://aws.amazon.com/powershell/ for details
 
+
+# Process optional command line arguments
+param (
+   [string]$namespace = "com.ft.editorial.cms.printpreview.test",
+   [string]$region = "eu-west-1"
+)
+
 $name = "Methode"
 $sleep = 5
 $cwd = $PSScriptRoot
@@ -22,4 +29,13 @@ if ( $rvalue ) {
 else {
   Write-Host "$name is not running, starting"
   & "C:\Program Files\EidosMedia\Methode\bin\Methode.exe" -server=printpreview1
+}
+
+Start-Sleep -s $sleep
+$rvalue = checkProcess $name
+if ( $rvalue ) {
+  . ${cwd}/cloudwatch_put.ps1 -value 1 -namespace "$namespace"
+}
+else {
+  . ${cwd}/cloudwatch_put.ps1 -value 0 -namespace "$namespace"
 }
